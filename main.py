587567c41c1811cf5b00ddd3b01dca539c731fcf
Maratik05 <muhumaalievm@gmail.com>
2023-11-04 16:21:42 +0300

    second commit

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
+ 
 app = Flask(__name__)
 
 @app.route('/')
-
-def hello():
-    return 'Hello word'
+def index():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return 'Information about autor'
+    return render_template('about.html')
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
 
-@app.route('/blog')
-def blog():
-    return 'This is blog'
+@app.route('/services')
+def services():
+    return render_template('services.html')
 
-@app.route('/user/<username>')
-def user_profile(username):
-    return f"Это профиль пользователя {username}"
+@app.route('/faqs')
+def faqs():
+    return render_template('faqs.html')
 
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True)
 
 
 
