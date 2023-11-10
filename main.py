fae2572ad271b69a9a7baf6dd413b505dd5ad249
Maratik05 <muhumaalievm@gmail.com>
2023-11-10 19:00:05 +0300

    fourth commit

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import sqlite3
+
 
  
 app = Flask(__name__)
+
+def create_table():
+    con = sqlite3.connect('products.db')
+    cursor = con.cursor()
+    # cursor.execute('''DROP TABLE news;''')
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS news(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    watch_brands VARCHAR,
+    price INTEGER,
+    water_resistance VARCHAR,
+    quality VARCHAR
+) ''')
+    con.commit()
+    cursor.close()
+    con.close()
+
+
+
 
 @app.route('/')
 def index():
@@ -13,8 +34,18 @@ def about():
 
 @app.route('/products')
 def products():
-    return render_template('products.html')
-
+    if request.method == 'GET':
+        return render_template('products.html')
+    else:
+        con = sqlite3.connect('products.db')
+        cursor = con.cursor()
+        query = cursor.execute('''INSERT INTO news(watch_brands,price,water_resistance,quality) VALUES(?,?,?,?)''')
+        query_data = (request.form.get('brand'), request.form.get('price'), request.form.get('water_resistance'), request.form.get('quality'))
+        cursor.execute(query,query_data)
+        con.commit()
+        cursor.close()
+        con.close() 
+        return 'Форма заполнена успешно.'
 @app.route('/services')
 def services():
     return render_template('services.html')
@@ -32,7 +63,8 @@ def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_table()
+    app.run(debug=True, port=8000)
 
 
 
